841ac165a2a20f10861a193439062811cddb0f7e
Kate Sugden <kate.sugden@madetech.com>
2025-01-27 16:02:31 +0000

    Add test for combining in different orders

--- probability.test.js
+++ probability.test.js
@@ -13,4 +13,11 @@ describe('Test Probabilities combined', () => {
     test('Probabilities combined together correctly', () => {
         expect(new Probability(1,2).combineWith(new Probability(1,2)).equals(new Probability(1,4))).toBe(true);
     })
+    test('Probabilities combined are order agnostic', () => {
+        const probabilityA = new Probability(1,3)
+        const probabilityB = new Probability(1,2)
+        const combination1 = probabilityA.combineWith(probabilityB)
+        const combination2 = probabilityB.combineWith(probabilityA)
+        expect(combination1.equals(combination2)).toBe(true);
+    })
 })
