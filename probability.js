933d55f8862f8cf5fb96e94188b5ee6859b89282
Dtzz81 <denise.turkova@madetech.com>
2025-01-27 15:42:18 +0000

    test probability combined passed

--- probability.js
+++ probability.js
@@ -1,9 +1,17 @@
 export class Probability{
     constructor(number, out_of) {
         this.value = number/out_of
+        this.number = number
+        this.out_of = out_of
+
       }
-    
+
     equals(p){
         return this.value == p.value;
+
     }
+    combineWith(p){
+        return new Probability(this.number * p.number, this.out_of * p.out_of);
+
+}
 }
--- probability.test.js
+++ probability.test.js
@@ -8,3 +8,9 @@ describe('Test Probability...', () => {
         expect(new Probability(1,2).equals(new Probability(1,3))).toBe(false);
     })
 })
+
+describe('Test Probabilities combined', () => {
+    test('Probabilities combined together correctly', () => {
+        expect(new Probability(1,2).combineWith(new Probability(1,2)).equals(new Probability(1,4))).toBe(true);
+    })
+})
